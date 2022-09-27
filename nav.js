9886db6c3649f24056d94b3acaf9fc16663ca3f9
Eteyen <114479164+Eteyen@users.noreply.github.com>
2022-09-27 10:35:42 +0100

    Project upload

---
+++ nav.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.navbar-emma').click(function(){
+        $('.nav-emma').toggleClass('visible');
+        $('body').toggleClass('cover-bg');
+    });
+});
---
+++ responsiveslides.js
@@ -0,0 +1,11 @@
+
+        $(document).ready(function() {
+          $(".rslides").responsiveSlides({
+              auto: true,
+              speed: 500,
+              timeout: 4000,
+              prevtext: "Prev",
+              nexttext: "Next",
+
+          });
+        });
